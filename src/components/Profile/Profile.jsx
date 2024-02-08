dc808d8e1d31658d0f6a6f734cc72421aef696bd
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-02-08 23:49:05 +0200

    feat: add profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+export const Profile = props => {
+  const {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  } = props;
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt={tag} />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
